bcd98fd62091f8e46fd9edb205dbd3319e46324a
ashu130698 <ashushukla299@gmail.com>
2025-07-26 00:38:07 +0530

    UseState with array,number,object and boolean

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,20 @@
-import StudentResult from './components/Dynamicstyle';
-import Footer from './components/footer';
-import Navbar from './components/navbar';
-import SampleEvent from './components/samplevent';
-import Slider from './components/Slider';
+import UseCase1 from "./components/UseState/UseCase1";
+import UseCase2 from "./components/UseState/UseCase2";
+import UseCase3 from "./components/UseState/UseCase3";
+import UseCase4 from "./components/UseState/UseCase4";
+import UseCase5 from "./components/UseState/UseCase5";
+
 
 function App() {
   return (
     <>
-    <StudentResult />
-{/*       
-      <Navbar />
-      <Slider />
-      
-
-      
-      <div className="container" style={{ paddingTop: '90px' }}>
-        
-        <h3 className="text-primary text-center bg-dark text-white p-3 mb-4">
-          Skill Academy
-        </h3>
+    <UseCase1 />
+    <UseCase2 />
+    <UseCase3 />
+    <UseCase4 />
+    <UseCase5 />
 
 
-        <h1>Welcome to my react app</h1>
-        <p>This is homepage content.</p>
-      </div>
-      <Footer /> */}
     </>
   );
 }
