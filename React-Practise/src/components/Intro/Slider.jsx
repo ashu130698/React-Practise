c0e395c3539de61edfe1ba19149369093ecb190a
ashu130698 <ashushukla299@gmail.com>
2025-07-27 00:11:02 +0530

    Convert React-Practise from submodule to normal folder

---
+++ React-Practise/src/components/Intro/Slider.jsx
@@ -0,0 +1,27 @@
+function Slider() {
+    return ( 
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://tse4.mm.bing.net/th/id/OIP.izpGSPU1ei8qirfBYSbDrAHaE7?pid=Api&P=0&h=180" height="300" class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="https://tse2.mm.bing.net/th/id/OIP.N4riAMowttuNWJKjwuwbTAHaEo?pid=Api&P=0&h=180" height="300" class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="https://tse1.mm.bing.net/th/id/OIP.lK-ja354astfQ_TMcSwwDgHaEK?pid=Api&P=0&h=180" height="300" class="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+     );
+}
+
+export default Slider;
