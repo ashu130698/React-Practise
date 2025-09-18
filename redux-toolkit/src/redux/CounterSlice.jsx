e3e994feec0cd33f2615c9a8cd041214937dcbf2
ashu130698 <ashushukla299@gmail.com>
2025-09-18 22:02:31 +0530

    Using RTK in redux

---
+++ redux-toolkit/src/redux/CounterSlice.jsx
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+    name: 'Counter',
+    initialState: {
+        count:0
+    },
+    reducers: {
+        increment: state => {
+            state.count+=1
+        },
+        decrement: state => {
+            state.count-=1
+        },
+        incrementByamount: (state, action) => {
+            state.count+=action.payload
+        },
+        decrementByamount: (state, action) => {
+            state.count-=action.payload
+        }
+    }
+})
+
+export const { increment, decrement, incrementByamount, decrementByamount } = counterSlice.actions
+export default counterSlice.reducer;
