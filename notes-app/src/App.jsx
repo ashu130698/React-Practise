eafec6bf2cfc8f8edf61ffd1abe79b36df8512da
ashu130698 <ashushukla299@gmail.com>
2025-09-25 22:04:34 +0530

    fix bug

---
+++ notes-app/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="h-screen flex justify-center items-center bg-blue-500">
+      <h1 className="text-white text-4xl font-bold">Tailwind is working!</h1>
+    </div>
+  );
+}
